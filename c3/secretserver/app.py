600b9e9092086423bbb1293fe5a7dce2fcfe088e
Jeremy Mill <jeremy.mill@floqast.com>
2021-04-15 08:08:35 -0400

    fixes to challenge 3

--- c3/secretserver/app.py
+++ c3/secretserver/app.py
@@ -4,19 +4,14 @@ import subprocess
 
 app = Flask(__name__)
 api = Api(app)
-# for TODOs
-todos = {}
 
 class Flag(Resource):
     def get(self):
         calling_ip = request.remote_addr
-        if calling_ip == "127.0.0.1":
-            return {'flag1': 'G4sZkDjp3iI'}
-        else:
-            return {}, 401
+        return {'flag1': 'final_flag_1337'}
 
 
-api.add_resource(Flag, '/flag')
+api.add_resource(Flag, '/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
